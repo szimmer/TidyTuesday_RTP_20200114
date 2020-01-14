c53ed0287dbb90930a56d36e749eb16c873b9020
Stephanie Zimmer <stephaniezimmer@gmail.com>
2020-01-13 19:27:43 -0500

    Initialize exploration program

---
+++ 01_DataExplore.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+# Get the Data
+
+dog_moves <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-12-17/dog_moves.csv')
+dog_travel <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-12-17/dog_travel.csv')
+dog_descriptions <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-12-17/dog_descriptions.csv')
+
---
+++ README.md
@@ -0,0 +1,3 @@
+https://www.meetup.com/rladies-rtp/events/267640728/
+
+Data: https://github.com/rfordatascience/tidytuesday/tree/master/data/2019/2019-12-17
